8cc7dc31901258f962cd252a41b95408d5ac34df
juanjo2506 <juanjose250699@gmail.com>
2024-04-19 11:21:36 -0500

    instagram correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
             <li><a href="https://github.com/juanjo2506" target="_blank"><FontAwesomeIcon icon={faGithub}/></a></li>
             <li><a href="https://www.facebook.com/jaason.voorhes/" target="_blank"><FontAwesomeIcon icon={faFacebook}/></a></li>
             <li><a href="https://www.linkedin.com/in/juan-jose-tellez-292698251/" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a></li>
-            <li><a href="https://www.linkedin.com/in/juan-jose-tellez-292698251/" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a></li>
+            <li><a href="https://www.instagram.com/juanjo2506/" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a></li>
         </ul>
         <img src={img} alt="" className='footer-img'/>
         <p className='Copyright'>Copyright©2024 JAPPWORKOUT</p>
